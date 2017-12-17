c2a00c9212789c0b0a0f4287d5e972de1b6ad939
Stefan Johnson <sj@scrimpycat.io>
2017-12-18 00:56:59 +1100

    Setup file header template

--- ios/honeycomb/AppDelegate.m
+++ ios/honeycomb/AppDelegate.m
@@ -1,10 +1,10 @@
-/**
- * Copyright (c) 2017, ZURASTA.
- * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree.
- */
+//
+// Copyright (c) 2017, ZURASTA.
+// All rights reserved.
+//
+// This source code is licensed under the license found in the
+// LICENSE file in the root directory of this source tree.
+//
 
 #import "AppDelegate.h"
 
