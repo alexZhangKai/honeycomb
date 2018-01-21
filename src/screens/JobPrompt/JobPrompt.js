41f4d28b8b9b7dd2a5c03c5b6ae0d61ab8e84363
Alex <alexzhang1931@gmail.com>
2018-01-21 16:29:00 +1100

    Add dismiss button in jobPrompt screen

--- src/screens/JobPrompt/JobPrompt.js
+++ src/screens/JobPrompt/JobPrompt.js
@@ -7,11 +7,30 @@ import JobList from '../../component/JobList';
 import styles from '../../config/styles';
 
 export default class JobPrompt extends Component<{}> {
-    componentDidMount(){
+    static navigatorButtons = {
+        rightButtons: [
+            {
+                title: 'Done',
+                id: 'done'
+            }
+        ]
+    };
+
+    constructor(props){
+        super(props);
         this.props.navigator.setDrawerEnabled({
             side: 'left',
             enable: false
         });
+        this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent.bind(this));
+    }
+
+    onNavigatorEvent(event){
+        if(event.type === 'NavBarButtonPress') {
+            if(event.id === 'done') {
+                this.props.navigator.dismissModal();
+            }
+        }
     }
 
     render(){
